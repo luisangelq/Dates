fec27a7a5c854600c9c0e2fcba3930dab20282ab
Luis Quiñones <luis.quinones@thebrainapps.com>
2020-09-21 01:28:48 -0500

    add run build

---
+++ build/precache-manifest.6988397a9e5b9054206217e0250e4787.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "963db4defcab92b81ebdf159af8b444c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19523a969843dc31dbe8",
+    "url": "/static/css/main.327c5b22.chunk.css"
+  },
+  {
+    "revision": "93d25f656e917bb39071",
+    "url": "/static/js/2.8ebad2c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8ebad2c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19523a969843dc31dbe8",
+    "url": "/static/js/main.f24f49cd.chunk.js"
+  },
+  {
+    "revision": "81e1fbdd9e963d37267e",
+    "url": "/static/js/runtime-main.a2fd21fb.js"
+  }
+]);
